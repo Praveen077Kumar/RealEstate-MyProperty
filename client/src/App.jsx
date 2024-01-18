bd75ca319cdf019a3c70a35aa07b2fc028b36dec
Praveen077Kumar <pk077prince@gmail.com>
2024-01-18 23:16:33 +0530

    Setup the project and install the dependencies configure all the things needed

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const App = () => {
+  return (
+      <h1 className='text-red-400'>hello</h1>
+  );
+}
+
+export default App;
