cdcfd14bf522e4704fb339e1895139a267c8f20b
Praveen077Kumar <pk077prince@gmail.com>
2024-01-20 00:21:25 +0530

    change name of userinfo in signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,11 +23,11 @@ export const signin = async (req, res, next) => {
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong Credentials! Try again."));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
-    const {password:pass,...rest} = validUser._doc;
+    const {password:pass,...restUserInfo} = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(rest);
+      .json(restUserInfo);
   } catch (error) {
     next(error);
   }
