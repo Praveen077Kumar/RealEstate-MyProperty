1bf8b681de0ddb9794274d1b4731cd0513119a0c
Praveen077Kumar <pk077prince@gmail.com>
2024-01-20 00:20:43 +0530

    createed signin route and accessToken generate functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,34 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup= async(req, res, next) => {
-   const {username,email, password} = req.body;
-   const hashedPassword = bcrypt.hashSync(password,10);
-   const newUser = new User({username, email, password:hashedPassword});
-   try {
-     await newUser.save();
-     res.status(201).json({message:"User Created successfully!"});
-   }
-   catch (error) {
-        next(error)
-   }
-  
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User Created successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials! Try again."));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const {password:pass,...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 import bcryptjs from 'bcryptjs';
 
 const router = new express.Router();
 
 router.post('/signup',signup)
+router.post('/signin',signin)
 
 
 export default router;
