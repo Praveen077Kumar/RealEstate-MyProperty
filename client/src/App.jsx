a9180d410b3d6cadf942fc01f00520ca3b3e1e9d
Praveen077Kumar <pk077prince@gmail.com>
2024-01-20 12:29:00 +0530

    implimented the redux toolkit , persistent redux for localstorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
-import Signin from "./pages/signin";
+import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,17 @@
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate= useNavigate();
+  const {loading, error} = useSelector((state)=> state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,7 +22,7 @@ const SignIn = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -26,16 +32,13 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
-      navigate('/')
+      dispatch(signInSuccess(data))
+      navigate("/");
     } catch (error) {
-        setLoading(false);
-        setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -69,7 +72,7 @@ const SignIn = () => {
           <span className="text-blue-700">Sign up </span>
         </Link>
       </div>
-       { error && <p className="text-red-600 mt-4">{error}</p>}
+      {error && <p className="text-red-600 mt-4">{error}</p>}
     </div>
   );
 };
