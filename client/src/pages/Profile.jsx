fc98e6e11b81a1004935a30ce84fdce2ab827a09
Praveen077Kumar <pk077prince@gmail.com>
2024-01-25 00:09:14 +0530

    added the profile page ui form

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,22 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import {useSelector} from 'react-redux'
 
 const Profile = () => {
+    const {currentUser} = useSelector((state)=> state.user)
     return (
-        <div>
-            Profile
+        <div className='p-3 max-w-lg mx-auto' >
+        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+        <form className='flex flex-col gap-4'>
+            <img src={currentUser.avatar} alt="profile-pic"  className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+            <input type="text" placeholder={currentUser.username} className='border p-3  rounded-lg' id='username'/>
+            <input type="email" placeholder={currentUser.email} className='border p-3  rounded-lg' id='email'/>
+            <input type="password" placeholder='password' className='border p-3  rounded-lg' id='password'/>
+            <button className='border p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-80 '>Update</button>
+        </form>
+
+        <div className='flex justify-between m-5'>
+            <span className='text-red-700 cursor-pointer'>Delete account</span>
+            <span className='text-red-700 cursor-pointer'>Sign-out</span>
+        </div>
         </div>
     );
 }
